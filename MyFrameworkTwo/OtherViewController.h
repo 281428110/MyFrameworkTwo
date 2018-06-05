6901f506446bb3bfeea955633d4f68bc9d0ed5cf
周明亮 <mingliang.zhou@bblink.cn>
2018-06-05 10:08:59 +0800

    fix

---
+++ MyFrameworkTwo/OtherViewController.h
@@ -0,0 +1,13 @@
+//
+//  OtherViewController.h
+//  MyFrameworkTwo
+//
+//  Created by 周明亮 on 2018/6/5.
+//  Copyright © 2018年 BBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherViewController : UIViewController
+
+@end
